f5221122b08228cc98f827d60e0d7ea1b92b4754
Ivan Khokhlov <khokhlovivan22@gmail.com>
2025-09-01 19:51:24 +0300

    доработанный mazefield.cpp

--- project/mazefield.cpp
+++ project/mazefield.cpp
@@ -27,6 +27,8 @@ void MazeField::draw()
     if (rows == 0) return;
     int cols = grid[0].size();
     // рисуем стены
+    QPen wallPen(QColor(110, 110, 110));
+    wallPen.setWidth(2);
     for (int r = 0; r < rows; ++r)
     {
         for (int c = 0; c < cols; ++c)
@@ -36,16 +38,17 @@ void MazeField::draw()
             int y = r*m_cellSize;
 
             if (cell.topWall)
-                addLine(x, y, x + m_cellSize, y);
+                addLine(x, y, x + m_cellSize, y, wallPen);
             if (cell.bottomWall)
-                addLine(x, y + m_cellSize,x + m_cellSize, y + m_cellSize);
+                addLine(x, y + m_cellSize,x + m_cellSize, y + m_cellSize, wallPen);
             if (cell.leftWall)
-                addLine(x, y, x, y + m_cellSize);
+                addLine(x, y, x, y + m_cellSize, wallPen);
             if (cell.rightWall)
-                addLine(x + m_cellSize, y, x + m_cellSize, y + m_cellSize);
+                addLine(x + m_cellSize, y, x + m_cellSize, y + m_cellSize, wallPen);
         }
     }
     addRect(m_model->m_Exit().second * m_cellSize, m_model->m_Exit().first * m_cellSize, m_cellSize, m_cellSize, QPen(Qt::NoPen), QBrush(Qt::green));//помечаем выход
+    setSceneRect(0, 0, cols * m_cellSize, rows * m_cellSize);//ДОРАБОТКА установка границ сцены
 }
 
 void MazeField::setPlayerPosition(int row, int col, QColor player_color)
@@ -54,6 +57,6 @@ void MazeField::setPlayerPosition(int row, int col, QColor player_color)
         m_playerItem = addRect(0, 0, m_cellSize, m_cellSize,QPen(Qt::NoPen),QBrush(player_color));
     else
         m_playerItem->setBrush(QBrush(player_color));
-    m_playerItem->setOpacity(0.7);
+    m_playerItem->setOpacity(0.65);
     m_playerItem->setPos(col * m_cellSize, row * m_cellSize);
 }
